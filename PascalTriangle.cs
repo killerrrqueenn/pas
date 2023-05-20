5d808e5c5e7253c4c4a583fab27ef91309a2a899
killerrrqueenn <123479656+killerrrqueenn@users.noreply.github.com>
2023-05-20 18:06:02 +0700

    Add files via upload

---
+++ PascalTriangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PascalTriangleGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalRows;
+
+            while (true)
+            {
+                Console.Write("Input the total number of rows: ");
+                totalRows = int.Parse(Console.ReadLine());
+
+                if (totalRows < 0)
+                {
+                    Console.WriteLine("Invalid row count for Pascal's triangle. Please try again.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int row = 0; row <= totalRows; row++)
+            {
+                int currentVal = 1;
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write(currentVal + " ");
+                    currentVal = currentVal * (row - col) / (col + 1);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
